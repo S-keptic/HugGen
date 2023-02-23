38acfc3a5a6ad1b2fd6209ee9e67894cf020a99a
skeptic <69573771+S-keptic@users.noreply.github.com>
2023-02-24 03:00:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+let genereteResponse = document.getElementById('btn')
+let download = document.getElementById('btn2');
+let img = document.getElementById('img');
+
+
+const gen = async ()=>{
+const data = await fetch('https://api.waifu.pics/sfw/hug')
+    let response = await data.json()
+    let result = response.url
+    img.innerHTML = `<img src=${result} alt="loading..">`
+    console.log(result)
+
+    return result;
+
+}
+
+let down = gen();
+
+const save = ()=>{
+    document.open(down)
+}      
+
+
+
+
+
+
+
+document.getElementById('img').innerHTML = gen();
+
+
+
+
+
+genereteResponse.onclick = async ()=>{
+
+
+    gen();
+
+}
+
+
